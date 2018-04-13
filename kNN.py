5975be7e2186a573b896a013c484761d60ee01f0
nda_adn <nguyendinhan97@gmail.com>
2018-04-13 16:02:15 +0700

    push KNN again

--- kNN.py
+++ kNN.py
@@ -28,19 +28,19 @@ def pearson(dataA, dataB):
             return -1
 
         dataLength = len(dataA)
-        #intersection = [i for i in range(dataLength) if dataA[i]!=0 and dataB[i]!=0]
-        intersection = [i for i in range(dataLength)]
+        intersection = [i for i in range(dataLength) if dataA[i]!=0 and dataB[i]!=0]
+        #intersection = [i for i in range(dataLength)]
         if len(intersection) == 0:
             return 0
 
-        #avgA = np.mean([i for i in dataA if i != 0])
-        #avgB = np.mean([i for i in dataB if i != 0])
-        avgA = np.mean([i for i in dataA])
-        avgB = np.mean([i for i in dataB])
+        avgA = np.mean([i for i in dataA if i != 0])
+        avgB = np.mean([i for i in dataB if i != 0])
+        #avgA = np.mean([i for i in dataA])
+        #avgB = np.mean([i for i in dataB])
         numerator = sum([(dataA[i] - avgA)*(dataB[i] - avgB) for i in intersection])
         deviationA = sqrt(sum([(dataA[i]-avgA)**2 for i in intersection]))
         deviationB = sqrt(sum([(dataB[i]-avgB)**2 for i in intersection]))
-        if(deviationA * deviationB)==0:
+        if(deviationA * deviationB) == 0:
             return 0
         return numerator/(deviationA*deviationB)
 
@@ -48,32 +48,60 @@ def pearson(dataA, dataB):
 def kNN(data, measure, k = None):
     #simulatedData = [[round(np.corrcoef(i,j)[0,1],3) for j in data] for i in data]
     simulatedData = [[round(measure(i,j),3) for j in data] for i in data]
+    #'''
     print("Similarity Data")
     for i in simulatedData:
         print(i)
+    #'''
+    print(k)
     newData = []
     for i in range(len(data)):
         newData.append([])
         for j in range(len(data[i])):
             if(data[i][j] == 0):
-                userCol = [(simulatedData[i][k], k) for k in range(len(simulatedData[i]))]
-                print(userCol)
+                #print("i,j = ", i,j)
+                userCol = [(simulatedData[i][index], index) for index in range(len(simulatedData[i]))]
+                #print("userCol")
                 userCol.sort(reverse = True)
-                print(userCol)
-                taken = userCol[1:k+1]
-                print(taken)
+                #print(userCol)
+                #taken = userCol[1:k+1]
+                taken = []
+                countk = 0
+                #print("k:",k)
+                for l in range(0,len(userCol)):
+                #    print(l)
+                #    print("data: ",data[userCol[l][1]][j])
+                    if userCol[l][1] == i:
+                        continue
+                    if (data[userCol[l][1]][j] != 0):
+                        countk += 1
+                        taken.append(userCol[l])
+                    if (countk == k):
+                        #print("noooowat")
+                        break
+                
+                #print("taken")
+                #print(taken)
                 #print(data)
                 
-                predictingMean = np.mean([k for k in data[i] if k != 0])
-                similarUserMean = [np.mean([l for l in data[k[1]] if l != 0]) for k in taken]
-                print("SimilarUserMean")
-                print(similarUserMean)
+                #k = len(taken)
+                predictingMean = np.mean([index for index in data[i] if index != 0])
+                similarUserMean = [np.mean([l for l in data[index[1]] if l != 0]) for index in taken]
+                
+                #print("SimilarUserMean")
+                #print(similarUserMean)
                 #print([data[i][taken[k][1]] for k in range(len(taken))])
-                print()
-                numerator = sum([simulatedData[i][taken[k][1]]*(data[taken[k][1]][j] - similarUserMean[k]) for k in range(len(taken))])
-                denominator = sum([simulatedData[i][taken[k][1]] for k in range(len(taken))])   
+                #print()
+                #print(simulatedData[i][taken[k][1]], data[taken[k][1]][j], similarUserMean[k])
+                numerator = sum([simulatedData[i][taken[index][1]]*(data[taken[index][1]][j] - similarUserMean[index]) for index in range(len(taken))])
+                #print(predictingMean)
+                denominator = sum([simulatedData[i][taken[index][1]] for index in range(len(taken))])
+                #print(denominator)
                 #print([simulatedData[i][taken[k][1]] for k in range(len(taken))])
                 #print()
+                if (denominator == 0):
+                    newData[i].append(0)
+                    continue
                 res = predictingMean + numerator/denominator
                 newData[i].append(round(res,3))
             else:
@@ -104,14 +132,19 @@ def readFile(fileString):
     return np.loadtxt(fileString, delimiter=" ", dtype = "int").tolist()
 
 if __name__ == "__main__":
-    fileString = r'F:\Tut\Sem6\Information System\testData\test1.txt'
+    fileString = r'F:\Tut\Sem6\Information System\testData\test3.txt'
     data = readFile(fileString)
     print("DATA:")
     for i in data:
         print(i)
 
     print("KNN Pearson:")
-    blah = kNN(data, cosine, 2)
+    blah = kNN(data, pearson, 2)
     for i in blah:
         print(i)
-
+    
+    print("\nKNN Cosine")
+    bleh = kNN(data, cosine, 2)
+    for i in bleh:
+        print(i)
+    
